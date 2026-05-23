0111a0b3541d34aa3a29b0fc3d6e3824bfab2e3f
anjali peddi <peddianjali40i@gmail.com>
2026-05-24 01:24:07 +0530

    completed jwt authentication backend

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,23 +1,14 @@
 const mongoose = require("mongoose");
 
 const connectDB = async () => {
-
   try {
+    await mongoose.connect(process.env.MONGO_URI);
 
-    const conn = await mongoose.connect(
-      process.env.MONGO_URI
-    );
-
-    console.log(`MongoDB Connected`);
-
+    console.log("MongoDB Connected");
   } catch (error) {
-
-    console.log(`DB Error: ${error.message}`);
-
+    console.log(error);
     process.exit(1);
-
   }
-
 };
 
 module.exports = connectDB;
