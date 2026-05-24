3ed24d1962304bf60bac93adc0d7edf333bd79bb
anjali peddi <peddianjali40i@gmail.com>
2026-05-24 12:14:17 +0530

    completed protected route middleware

--- backend/routes/authRoutes.js
+++ backend/routes/authRoutes.js
@@ -7,10 +7,13 @@ const {
   loginUser
 } = require("../controllers/authController");
 
+const { protect } = require("../middleware/authMiddleware");
 // REGISTER ROUTE
 router.post("/register", registerUser);
 
 // LOGIN ROUTE
 router.post("/login", loginUser);
-
+router.get("/profile", protect, (req, res) => {
+  res.json(req.user);
+});
 module.exports = router;
